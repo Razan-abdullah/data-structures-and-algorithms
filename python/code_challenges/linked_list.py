2984b399efedab955ca24f6358b82b647ca44ca0
Razan-abdullah <razanaalabdallah017@gmail.com>
2022-10-22 21:06:21 +0000

    d

--- python/code_challenges/linked_list.py
+++ python/code_challenges/linked_list.py
@@ -1,16 +1,49 @@
+# Write here the code challenge solution
+class Node:
+
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def append(self, value):
+#        this function to add new node
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = node
+    def delete_node(self, value):
+        if self.head is None:
+            return
+        if self.head.value == value:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next is not None:
+            if current.next.value == value:
+                current.next = current.next.next
+                return
+            current = current.next
+    def printAll(self):
 
+        # this function for print and return new list
 
-class TargetError:
-    pass
+        list=[]
+        if self.head is None :
+            print("The linked list is empty")
+        else :
+            current = self.head
+            while current is not None:
+                list.append(current.value)
+                print(current.value)
+
+                current = current.next
+        print(list)
+        return list
